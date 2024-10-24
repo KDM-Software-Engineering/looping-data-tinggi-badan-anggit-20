c76f82c468496b2256453cf2772dc2c7e46e8e7b
anggit-20 <geluhanggit20@gmail.com>
2024-10-24 14:10:08 -0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function runner() {
 
-  // TULIS SCRIPT KAMU DI SINI
+  let tinggiBadan = [150, 155, 148, 165, 170, 168, 166]
+  let namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
 
-
-
-
-
-
-  /// JANGAN UBAH SELAINNYA
+  for(let i = 0; i <tinggiBadan.length; i++){
+    console.log(`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiBadan[i]}cm`);
+  }
 }
+runner()
+
+
 
 module.exports = runner
